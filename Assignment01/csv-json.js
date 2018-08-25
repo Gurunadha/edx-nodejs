9eacd497a2da55c976381546cd1f6e7c877a591f
vijay <vijay@tandemarc.com>
2018-08-25 14:27:21 +0530

    second commit

--- Assignment01/csv-json.js
+++ Assignment01/csv-json.js
@@ -1,26 +1,28 @@
 const fs = require('fs')
 const path = require('path')
+    //By using csv to json node module
     // const csvToJson = require('csvtojson')
     // csvToJson()
     //     .fromFile('customer-data.csv')
     //     .then((jsonObj) => {
     //         console.log(jsonObj)
     //     })
+
+//By using core node modules
 fs.readFile('customer-data.csv', 'utf8', (error, data) => {
     if (error)
         return console.log(error)
     let json = []
+        //converting each row of csv to an array
     let dataArray = data.split('\n')
-        //console.log(dataArray)
+        //seperating the header content of csv
     let headers = dataArray.shift().replace(/\r/g, '').split(',')
-    console.log(headers)
-        //console.log(dataArray)
     dataArray.forEach(row => {
         let obj = {}
         row = row.replace(/\r/g, '')
         let rowArray = row.split(',')
-            //console.log(rowArray)
         for (let i = 0; i < headers.length; i++) {
+            //object to hold a row csv data in key-value pairs
             obj[headers[i]] = rowArray[i]
         }
         json.push(obj)
@@ -29,7 +31,7 @@ fs.readFile('customer-data.csv', 'utf8', (error, data) => {
     fs.writeFile(path.join(__dirname, 'customer-data.json'), JSON.stringify(json), 'utf8', (error) => {
         if (error)
             return console.error('erro while writing', error)
-                //fs.close()
+        console.log('conversion of csv to json is completed')
     })
 })
 console.log('reading file started')
